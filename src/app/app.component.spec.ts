06e5a863862ab8b19004bd0dd198c0e86dd6a95a
Kriss Cardenas <kriss_96@hotmail.com>
2020-04-16 02:05:29 -0400

    Premier commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lhsdb-lottery'`, () => {
+  it(`should have as title 'mon-application'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lhsdb-lottery');
+    expect(app.title).toEqual('mon-application');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('lhsdb-lottery app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mon-application app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lhsdb-lottery';
+export class AppComponent implements OnInit {
+  title = 'Lotterie 2020';
+  entries = [];
+  accumulatedWeight = 0.0;
+  actualStandings1 = [];
+  actualStandings2 = [];
+
+  results: string[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    /*
+    1- Ottawa 18,5% (185)
+2- Vegas 13,5% (135)
+3- Detroit 11,5% (115)
+4- Nashville 9,5% (95)
+5- Philadelphie 8,5% (85)
+6- Chicago 7,5% (75)
+7- Winnipeg 6,5% (65)
+8- Tampa Bay 6% (60)
+9- Montréal 5% (50)
+10- NYIslanders 3,5% (35)
+11- Toronto 3% (30)
+12- Los Angeles 2,5% (25)
+13- Vancouver 2% (20)
+14- San Jose 1,5% (15)
+15- Caroline 1% (10)*/
+
+    this.addEntry(1, "Ottawa", 18.5);
+    this.addEntry(2, "Vegas", 13.5);
+    this.addEntry(3, "Detroit", 11.5);
+    this.addEntry(4, "Nashville", 9.5);
+    this.addEntry(5, "Philadelphie", 8.5);
+
+    this.addEntry(6, "Chicago", 7.5);
+    this.addEntry(7, "Winnipeg", 6.5);
+    this.addEntry(8, "Tampa Bay", 6.0);
+    this.addEntry(9, "Montréal", 5.0);
+    this.addEntry(10,"New York Islanders", 3.5);
+
+    this.addEntry(11,"Toronto", 3.0);
+    this.addEntry(12,"Los Angeles", 2.5);
+    this.addEntry(13,"Vancouver", 2);
+    this.addEntry(14,"San Jose", 1.5);
+    this.addEntry(15,"Caroline", 1);
+  }
+
+  onDraft(): void {
+
+    if (this.results.length >= 3 || this.results.length >= this.entries.length) {
+      return;
+    }
+
+    let randomTeam = this.getRandomTeam();
+    if (!this.results.includes(randomTeam)) {
+      this.results.push(randomTeam);
+    }
+  }
+
+  addEntry(rank: number, team: string, weight: number) {
+    this.accumulatedWeight += weight;
+    this.entries.push({ rank: rank, name: team, accumulatedWeight: this.accumulatedWeight });
+
+    if (this.actualStandings1.length < 8) {
+      this.actualStandings1.push(rank + "- " + team + "(" + weight + "%)")
+    }
+    else {
+      this.actualStandings2.push(rank + "- " + team + "(" + weight + "%)")
+    }
+  }
+  // splice
+  /*
+          add  remove  start  end
+    push    X                   X
+    pop           X            X
+    unshift    X             X
+    shift           X      X
+  */
+
+
+  getRandomTeam(): string {
+    var r = Math.random() * this.accumulatedWeight;
+    console.log("Valeur pigée " + r + " sur " + this.accumulatedWeight);
+    // this.entries.find(entry.accumulatedWeight >= r)
+    return this.entries.find(function (entry) {
+      return entry.accumulatedWeight >= r;
+    }).name;
+  }
 }
